8b71e62f34d4304322834491f687adebedb9a53d
LoSk-p <alki.spb@mail.ru>
2021-08-17 15:32:02 +0300

    fix create user

--- lease_control/scripts/create_user.py
+++ lease_control/scripts/create_user.py
@@ -1,13 +1,12 @@
 from user_control import UserControl
 import sys
 
-# python3 create_user.py email ssh-pubkey
+# python3 create_user.py ssh-pubkey
 
-email = sys.argv[1]
-key = sys.argv[2]
+key = sys.argv[1]
 uc = UserControl()
 user, passw = uc.create_user_pass()
 user1, passw1 = uc.create_user_pass()
-uc.add_user_core(user, passw1, email, key)
+uc.add_user_core(user, passw1, key)
 uc.add_user_spot(user, passw)
 uc.estop_pub.publish("press allow")
